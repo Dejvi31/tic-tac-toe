6dbeac9ef900895d136afd5c6526b81af5e36f47
dejvi <dejviepadejvi@gmail.com>
2023-04-13 14:09:02 +0200

    shtuam projektin e mbaruar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Game from './Game'
+
+const App = () => {
+  return (
+    <div className='container'>
+    <div className='board'>
+      <Game />
+    </div>
+    </div>
+  )
+}
+
+export default App
